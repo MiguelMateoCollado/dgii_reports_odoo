27f3636c322956ac3d171ca8fb7af3c69d86770d
Miguel Angel Mateo Collado <mm@prixet.com>
2026-01-20 10:53:42 -0400

    init del modulo

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Dgii Reports',
+    'version': '1',
+    'category': 'accounting/module',
+    'summary': 'Sistema de la dgii para los reportes y demas necesidades en odoo.',
+    'description': "",
+    'depends': [
+       "accounting"
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
